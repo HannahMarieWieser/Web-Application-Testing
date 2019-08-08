7c08c3198ad5bfd706489994585e5946442aae78
HannahMarieWieser <nikauli@gmail.com>
2019-08-08 18:18:15 -0400

    fixing state errors

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -8,8 +8,8 @@ import './App.css';
 
 
   //------------State Rules---------------
-// -[] balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
-//-[] balls and strikes reset to 0 when a `hit` is recorded.
+
+
 //-[] Badclick !!! a `foul` increases strikes up to 2. 
   //With no strikes, a foul makes it 1 strike. 
   //With 1 strike, a foul makes it 2 strikes. 
@@ -25,42 +25,49 @@ function Dashboard(){
   const [hits, setHits] = useState({hitCounter: 0}) //hitclick //DONE
   const [fouls, setFouls] = useState({foulCounter: 0}) //badclick
 
-  const handleHitClick = event => {
+  // -[] balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
+  //-[] balls and strikes reset to 0 when a `hit` is recorded.
+  console.log('balls', balls)
+  if (balls.ballCounter === 5 || strikes.strikeCounter === 4 || hits.hitCounter === 1){
+    setBalls({ballCounter: 0})
+    setStrikes({strikeCounter: 0})
+  }
+
+ 
+
+
+  const handleHitClick = () => {
     const newHitValue = hits.hitCounter + 1
     setHits({hitCounter: newHitValue})
-    event.preventDefault();
     console.log('hit was clicked')
   }
 
-  const handleBallClick = event => {
+  const handleBallClick = () => {
     const newBallValue = balls.ballCounter + 1
     setBalls({ballCounter: newBallValue})
-    event.preventDefault();
     console.log('Ball was clicked')
+    
   }
 
-  const handleStrikeClick = event => {
+  const handleStrikeClick = () => {
     const newStrikeValue = strikes.strikeCounter + 1
     setStrikes({strikeCounter: newStrikeValue})
-    event.preventDefault();
-    console.log('hit was clicked')
+    console.log('strike was clicked')
   }
 
-
-  const handleBadClick = event => {
+  const handleBadClick = () => {
     const newFoulValue = fouls.foulCounter + 1
     setFouls({foulCounter: newFoulValue})
-    event.preventDefault();
-    console.log('hit was clicked')
+    console.log('foul was clicked')
   }
 
 
  return (
     <>
-      <button onClick={handleStrikeClick} className='button' > Strike </button>
-      <button onClick={handleBallClick} className='button'> Ball </button>
-      <button onClick={handleBadClick} lassName='button'> Foul </button> 
-      <button onClick={handleHitClick} className='button'> Hit </button>
+      <button onClick={()=>handleStrikeClick()} className='button' > Strike </button>
+      <button onClick={()=>handleBallClick()} className='button'> Ball </button>
+      <button onClick={()=>handleBadClick()} lassName='button'> Foul </button> 
+      <button onClick={()=>handleHitClick()} className='button'> Hit </button>
 
       {balls.ballCounter}
       {strikes.strikeCounter}
@@ -70,10 +77,8 @@ function Dashboard(){
     
     
     </>
-
-
-
- )
+  )
+  
 }
 
 function App() {
